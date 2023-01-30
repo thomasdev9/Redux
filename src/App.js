5b2c5a95c1a2b32d377c8c0799da334cd2c7d48c
thomasdev9 <makis7olymp@yahoo.gr>
2023-01-30 20:31:39 +0200

    React Redux

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+        <HooksCakeContainer />
+        <IceCreamContainer />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
